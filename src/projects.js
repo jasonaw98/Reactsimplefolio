154d468ec624f4cce6f0332838925d41c9dcfca9
jasonaw <jason_aw1998@hotmail.com>
2023-03-28 23:23:20 +0800

    Add Projects

---
+++ src/projects.js
@@ -0,0 +1,29 @@
+const projects = [
+    {
+      title: "Project Title 0",
+      description:
+        "Describe the project being very specific, you can use the Twitter standard: no more than 280 characters: complement the information: the skills learned or reinforced in its realization and how you faced it, prove to be proactive in the search for solutions.",
+      liveLink: "https://domain-starter-project.jasonaw98.repl.co/",
+      sourceLink: "https://domain-starter-project.jasonaw98.repl.co/",
+      imageSrc: "assets/project.jpg",
+    },
+    {
+      title: "Project Title 1",
+      description:
+        "Demonstrate in this description the skills of a programmer: such as having commitment, having perseverance and accepting alternative solutions. Remember that being a portfolio you are not selling the project, you are selling yourself, it reflects the resources used: Frameworks, libraries, platforms, etc.",
+      liveLink: "https://domain-starter-project.jasonaw98.repl.co/",
+      sourceLink: "https://domain-starter-project.jasonaw98.repl.co/",
+      imageSrc: "assets/project.jpg",
+    },
+    {
+      title: "Project Title 2",
+      description:
+        "If the project was collaborative, reflect it in this description, that will demonstrate communication and/or leadership skills. Additionally, if you made use of the mastery of a second language, it will reflect on you professionalism.",
+      liveLink: "https://domain-starter-project.jasonaw98.repl.co/",
+      sourceLink: "https://domain-starter-project.jasonaw98.repl.co/",
+      imageSrc: "assets/project.jpg",
+    },
+  ];
+  
+  export default projects;
+  
